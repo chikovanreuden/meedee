79e13d22bc366c32761d78e474394f20217b51c0
Chiko <akanoskilla@live.de>
2025-04-24 12:54:18 +0200

    Refactor code.py
    Implemented the onboard WS2812 LED via Lib "NeoPixel".
    Implemented Functionality to the onboard WS2812 LED.
    Implemented a 128x32 OLED with i2c via Lib "SSD1306_I2C".
    Implemented Functionality to the 128x32 OLED.
    Knobs: class MyKnobs now has the new Property "channel" to send the same CC on different channels.
    Added more Knobs (POTs) and more Buttons.

--- code.py
+++ code.py
@@ -18,23 +18,47 @@ import adafruit_midi  # MIDI protocol encoder/decoder library
 from adafruit_midi.control_change import ControlChange
 from adafruit_midi.note_off import NoteOff
 from adafruit_midi.note_on import NoteOn
-
 from adafruit_debouncer import Debouncer
+from adafruit_ssd1306 import SSD1306_I2C
+
+from rainbowio import colorwheel
+import neopixel
+
+pixel_pin = board.GP23
+num_pixels = 1
+neo = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+ORANGE = (255, 200, 0)
+NIL = (0, 0, 0)
+
+neo.fill(RED)
+neo.show()
+
+i2c = busio.I2C(scl=board.GP17, sda=board.GP16)
+oled = SSD1306_I2C(128, 32, i2c) 
+oled.fill(0)
+oled.text('MEEDEE', 48, 0, 1)
+oled.text('WEEEEE', 48, 20, 1)
+oled.show()
 
 # pick your USB MIDI out channel here, 1-16
 MIDI_USB_channel = 1
-# pick your classic MIDI channel for sending over UART serial TX/RX
-# CLASSIC_MIDI_channel = 2
 
 midi_usb = adafruit_midi.MIDI(
     midi_out=usb_midi.ports[1],
     out_channel=MIDI_USB_channel - 1
 )
-#  use DIN-5 or TRS MIDI jack on TX/RX for classic MIDI
-# midi_uart = busio.UART(board.TX, board.RX, baudrate=31250, timeout=0.001)  # initialize UART
-# classic_midi = adafruit_midi.MIDI(
-#     midi_out=midi_uart, midi_in=midi_uart, out_channel=CLASSIC_MIDI_channel - 1, debug=False
-# )
+
+max_channels =4
+
+midi_channels = [];
 
 led = digitalio.DigitalInOut(board.GP25)  # activity indicator
 led.direction = digitalio.Direction.OUTPUT
@@ -75,16 +99,39 @@ class MyButton:
 
 MyButtons = [
     MyButton(
-        board.GP21,
-        60,
+        board.GP1,
+        61,
+    ),
+    MyButton(
+        board.GP2,
+        62,
+    ),
+    MyButton(
+        board.GP3,
+        63,
+    ),
+    MyButton(
+        board.GP4,
+        64,
+    ),
+    MyButton(
+        board.GP5,
+        65,
+    ),
+    MyButton(
+        board.GP6,
+        66,
+    ),
+    MyButton(
+        board.GP18,
+        69,
     )
 ]
 
 class MyREncoder:
     pinA = None
     pinB = None
-    button_state = None
-    last_position = None
+    last_position = 0
     encoder = None
     def __init__(self, pinA, pinB):
         self.pinA = pinA
@@ -93,8 +140,8 @@ class MyREncoder:
 
 MyREncoders = [
     MyREncoder(
-        board.GP2,
-        board.GP3
+        board.GP19,
+        board.GP20
     )
 ];
 
@@ -104,25 +151,52 @@ class MyKnob:
     cc_range = (0, 127)
     cc_value = (0, 0)
     cc_value_last = (0, 0)
-    def __init__(self, pin, cc_number, cc_range):
+    channel = 0
+    def __init__(self, pin, cc_number, cc_range, channel):
         self.pin = pin
         self.cc_number = cc_number
         self.cc_range = cc_range
         self.adc = AnalogIn( pin )
+        self.channel = channel
 
 MyKnobs = [
     MyKnob(
         board.GP26,
         7,
-        (0, 127)
+        (0, 127),
+        0
+    ),
+    MyKnob(
+        board.GP27,
+        7,
+        (0, 127),
+        1
+    ),
+    MyKnob(
+        board.GP28,
+        7,
+        (0, 127),
+        2
     )
 ]
 
-print("   USB MIDI channel: {}".format(MIDI_USB_channel))
-# print("   TRS MIDI channel: {}".format(CLASSIC_MIDI_channel))
+def knob_neo( value ):
+    if not value:
+        return (0,0,0)
+    red = 0
+    green = 255
+    if value <= 63:
+        red = ( 256 / 64 ) * value
+        green = 255
+    elif value > 63:
+        red = 255
+        green = 255 - (( 256 / 64 ) * (value-64))
+    print((red, green, 0));
+    return (red, green, 0)
+
+
 
 while True:
-
     for btn in MyButtons:
         btn.button.update()
         
@@ -132,10 +206,10 @@ while True:
         #    print("pressed")
         if btn.button.fell:
             midi_usb.send( NoteOn( btn.note, 127 ) )
-            #print('Just pressed')
+            print(' pressed {0}'.format(btn.note))
         if btn.button.rose:
             midi_usb.send( NoteOff( btn.note, 0 ) )
-            #print('Just released')
+            print('released {0}'.format(btn.note))
     for rencoder in MyREncoders:
         current_position = rencoder.encoder.position
         position_change = current_position - rencoder.last_position
@@ -146,7 +220,6 @@ while True:
         elif position_change < 0:
             for _ in range(-position_change):
                 print(current_position)
-            
         rencoder.last_position = current_position
     for knob in MyKnobs:
         knob.cc_value = range_index(
@@ -155,14 +228,18 @@ while True:
             knob.cc_value[0],
             knob.cc_value[1],
         )
-        #print( knob.cc_value )
-        if knob.cc_value != knob.cc_value_last:  # only send if it changed
-            # Form a MIDI CC message and send it:
-            midi_usb.send(ControlChange(knob.cc_number, knob.cc_value[0] + knob.cc_range[0]))
-            # classic_midi.send(
-            #     ControlChange(knob_cc_number[i], knob_cc_value[i][0] + knob_cc_range[i][0])
-            # )
+        if knob.cc_value != knob.cc_value_last:
+            midi_usb.send(
+                ControlChange(
+                    knob.cc_number,
+                    knob.cc_value[0] + knob.cc_range[0]
+                ),
+                knob.channel
+            )
             knob.cc_value_last = knob.cc_value
+            print("knob {0}".format(knob.channel))
+            neo.fill( knob_neo( knob.cc_value[0] ) )
+            neo.show()
             led.value = True
     time.sleep(0.01)
     led.value = False
